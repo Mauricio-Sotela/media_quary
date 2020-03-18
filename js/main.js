dacbad8d95fab057f92193e2a8ebaf9504dea1f2
sotela88 <j-ms-27@hotmail.com>
2020-03-18 13:59:38 +0100

    media_quary

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ const berger = document.querySelector(".m");
 console.log(berger);
 
 berger.addEventListener("click", () => {
- document.body.classList.toggle("berger");
- 
+  document.body.classList.toggle("berger");
 });
 
+const comprobarAncho = () => {
+  if (window.innerWidth > 601) {
+    document.body.classList.add('berger');
+  }
+};
+window.addEventListener('resize', () => {
+	comprobarAncho();
+});
