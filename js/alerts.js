5b397666e59cacdb70b346fcf04a695977ec3a67
sotela88 <j-ms-27@hotmail.com>
2020-03-18 20:18:14 +0100

    media_quary

---
+++ js/alerts.js
@@ -0,0 +1,31 @@
+(async () => {
+  const { value: name } = await Swal.fire({
+    title: "Welcome!",
+    //text:
+    html: "<b> Mauricio Responsive Website </b>",
+    icon: "success",
+    confirmButtonText: "Go to the page",
+    backdrop: true,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    stopKeydownPropagation: true,
+    input: "text",
+    inputPlaceholder: "Write  your name"
+  });
+
+  if (name) {
+    Swal.fire({
+      title: "Hello!",
+      html: `<b> ${name}</b>`,
+      backdrop: true,
+      timer: 6000,
+      timerProgressBar: true,
+      toast: true,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+      stopKeydownPropagation: true
+    });
+  }
+})();
