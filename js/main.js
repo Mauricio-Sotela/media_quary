0f009c8db36a8db0d0eefa5076557e629b4c0d38
sotela88 <j-ms-27@hotmail.com>
2020-03-19 13:08:18 +0100

    media_query

--- js/main.js
+++ js/main.js
@@ -1,32 +1,27 @@
-
 ///////////function//////////////
 const berger = document.querySelector(".m");
 berger.addEventListener("click", () => {
   document.body.classList.toggle("berger");
-//////////X function/////////////
-document.querySelector(".x1").classList.toggle("ch1")
-document.querySelector(".x2").classList.toggle("ch2")
-document.querySelector(".x3").classList.toggle("ch3")
-
-
+  //////////X function/////////////
+  document.querySelector(".x1").classList.toggle("ch1");
+  document.querySelector(".x2").classList.toggle("ch2");
+  document.querySelector(".x3").classList.toggle("ch3");
 });
 
 ////////////function/////////////
-window.addEventListener('resize', () => {
-	 if (window.innerWidth > 601) {
-    document.body.classList.add('berger');
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 601) {
+    document.body.classList.add("berger");
+     //////////X function/////////////
+    document.querySelector(".x1").classList.remove("ch1");
+    document.querySelector(".x2").classList.remove("ch2");
+    document.querySelector(".x3").classList.remove("ch3");
   }
 });
 
-
-
-
-
-
- ///////////function////////////////========>//not working ATM
+///////////function////////////////========>//not working ATM
 // window.addEventListener('scroll', () => {
 //   document.body.classList.add('berger');
 //   console.log("ggggg");
-  
+
 //  });
-  
